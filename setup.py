8346aa2a2fc3b894781dbe4e8023450651c61c20
Ian Good <ian.good@rackspace.com>
2013-10-20 13:22:32 -0400

    upped version number

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='slimta',
-      version='0.1.13',
+      version='0.1.14',
       author='Ian Good',
       author_email='icgood@gmail.com',
       description='Configurable MTA based on the python-slimta library.',

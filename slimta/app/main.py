a324dfe448534c4fd481d9dfbe6a5314f8e2a433
Ian Good <icgood@gmail.com>
2020-02-24 18:30:02 -0500

    Add options to disable edges and relays

--- slimta/app/main.py
+++ slimta/app/main.py
@@ -20,6 +20,8 @@
 #
 
 import os
+import os.path
+import sys
 from argparse import ArgumentParser
 
 os.environ['GEVENT_RESOLVER'] = 'ares'
@@ -32,18 +34,25 @@ from .state import SlimtaState
 def parse_args():
     argparser = ArgumentParser(description='Configurable MTA based on the python-slimta libraries.')
     argparser.add_argument('--version', action='version', version='%(prog)s '+__version__)
-    argparser.add_argument('-c', '--config', metavar='FILE', default=None,
-                    help='Specifies a configuration file to read. If not given, the default locations ($HOME/.slimta/slimta.yaml, /etc/slimta/slimta.yaml) are checked.')
-    argparser.add_argument('-n', '--process-name', metavar='NAME', default=None,
-                    help='Use the process sub-section NAME for configuration. By default, the name of the executable is used.')
     argparser.add_argument('-a', '--no-daemon', dest='attached', action='store_true',
-                    help='Override configs and force the process to remain attached to the terminal.')
+                           help='Force the process to remain attached to the terminal.')
     argparser.add_argument('-d', '--daemon', dest='attached', action='store_false',
-                    help='Override configs and force the process to daemonize.')
+                           help='Force the process to daemonize.')
     argparser.add_argument('-p', '--pid-file', metavar='FILE', default=None,
-                    help='Store process ID in FILE during execution.')
-    argparser.set_defaults(attached=None)
+                           help='Store process ID in FILE during execution.')
 
+    default_process_name = os.path.basename(sys.argv[0])
+    group = argparser.add_argument_group('config options')
+    group.add_argument('-c', '--config', metavar='FILE', default=None,
+                       help='Specifies a configuration file to read. If not given, the default locations ($HOME/.slimta/slimta.yaml, /etc/slimta/slimta.yaml) are checked.')
+    group.add_argument('-n', '--process-name', metavar='NAME', default=default_process_name,
+                       help='Use the process sub-section NAME for configuration. (default: %(default)s)')
+    group.add_argument('--no-edge', action='store_true',
+                       help='Ignore all configured edges.')
+    group.add_argument('--no-relay', action='store_true',
+                       help='Ignore all configured relays.')
+
+    argparser.set_defaults(attached=None)
     return argparser, argparser.parse_args()
 
 
